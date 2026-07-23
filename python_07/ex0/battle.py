b6324389f90b3303cec308c31e7eb0093d88ab21
Sefa Kartav <sekartav@k1m19s06.42istanbul.com.tr>
2026-07-23 16:24:44 +0300

    python6 is started

---
+++ python_07/ex0/battle.py
@@ -0,0 +1,40 @@
+import abc
+
+class Creature(abc.ABC):
+    @abc.abstractmethod
+    def attack(self):
+        pass
+
+    def describe():
+        print("")
+
+
+class Flameling(Creature):
+    def attack(self):
+        return("The Flameling is attacking.")
+    
+class Pyrodon(Creature):
+    def attack(self):
+        return("The Pyrodon is attacking")
+    
+class Aquabub(Creature):
+    def attack(self):
+        return("The Aquabub is attacking")
+    
+class Tarragon(Creature):
+    def attack(self):
+        return("The Tarragon is attacking")
+
+
+class CreatureFactory(abc.ABC):
+    @abc.abstractmethod
+    def create_base(self):
+        pass
+
+    @abc.abstractmethod
+    def create_evolved(self):
+        pass
+
+class FlameFactory(CreatureFactory):
+    def create_flame(self) -> Creature:
+        return Flameling()
