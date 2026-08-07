b350aabd74b425eaffa38100435ebd4d730001c3
Sefa Kartav <sekartav@k1m05s04.42istanbul.com.tr>
2026-08-07 15:29:15 +0300

    python8/ex1 is finissh maybe

---
+++ python_08/ex1/loading.py
@@ -0,0 +1,55 @@
+import sys
+import importlib
+
+def check_library() -> bool:
+    packages: dict[str, str] = {
+        "numpy": "Numeric computation ready",
+        "pandas": "Data manipulation ready",
+        "matplotlib": "Visualization redy",
+        "requests": "Network acces ready"
+
+    }
+    good: bool = True
+    print("Cecking dependencies:")
+
+    for pkc, message in packages.items():
+        try:
+            module = importlib.import_module(pkc)
+            version: str = getattr(module, "__version__", "unkown")
+            print(f"[OK] {pkc} ({version}) - {message}")
+        except ImportError:
+            print(f"[ERROR] Missing dependency: {pkc}")
+            print(f"Hint: Run 'pip install {pkc}' or 'poetry install'")
+            good = False
+
+    return good
+
+
+def analyze_matrix() -> None:
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    print("\nAnalyzing Matrix data...")
+    print("Processing 1000 data points...")
+
+    data: np.ndarray = np.random.rand(1000, 2)
+    df: pd.DataFrame = pd.DataFrame(data, columns=["X_Coord", "Y_Coord"])
+    print("Generating visualization...")
+    plt.figure(figsize=(8, 6))
+    plt.scatter(df["X_Coord"], df["Y_Coord"], color='green', alpha=0.5)
+    plt.title("Matrix Anomaly Detection")
+    filename: str = "matrix_analysis.png"
+    plt.savefig(filename)
+    print("Analysis complete!")
+    print(f"Results saved to: {filename}")
+
+
+if __name__ == "__main__":
+    print("LOADING STATUS: Loading programs...")
+    if not check_library():
+        print("\n[SYSTEM GLARE] Cannot "
+              "proceed without required modules.")
+        sys.exit(1)
+
+    analyze_matrix()
